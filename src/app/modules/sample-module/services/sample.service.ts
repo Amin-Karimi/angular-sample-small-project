f8439f3d073e977761ab8eea05fd87988adad822
Amin-Karimi <amin.karimi.sisakht@gmail.com>
2024-09-27 10:25:42 +0330

    sample module

---
+++ src/app/modules/sample-module/services/sample.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SampleService {
+
+  constructor() { }
+
+  sample(): boolean {
+    return true
+  }
+}
